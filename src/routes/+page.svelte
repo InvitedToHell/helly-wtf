3a17fb07854c351aa99d5822284cec5ace820d04
InvitedToHell <BrianDETeam@gmail.com>
2023-06-30 18:19:30 +0200

    FirstRelease

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body class="bg-base text-text">
+		<div class="h-screen heading flex items-center justify-center">
+            <h1 class="text-white animate__animated animate__fadeInLeftBig">Helly</h1>
+			<section id="section01" class="demo text-text">
+				<a href="#section02"><span></span>
+			  </section>
+        </div>
+		<div class="h-screen flex text items-center justify-center bg-mantle">
+			<dir class="grid grid-cols-1 animate__animated animate__fadeInLeftBig">
+				<dir class="telegram">
+					<a href="https://t.me/invitedtohell">telegram</a>
+				</dir>
+				
+				<dir class="github">
+					<a href="https://github.com/invitedtohell">github</a>
+				</dir>
+
+				<dir class="twitter">
+					<a href="https://twitter.com/InvitedToHell">twitter</a>
+				</dir>
+
+				<dir class="email">
+					<a href="emailto:info@helly.wtf">email</a>
+				</dir>
+
+				<dir class="planetmc">
+					<a href="https://www.planetminecraft.com/member/invitedtoheaven/">planetmc</a>
+				</dir>
+			</dir>
+        </div>
+	</body>
+
+
