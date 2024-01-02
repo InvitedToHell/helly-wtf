0a39539e6dc72a28a5e7acde1b613e0ee08ca628
InvitedToHell <BrianDETeam@gmail.com>
2024-01-02 02:01:49 +0100

    Changed fonts and fixed some issues ig? Too long ago x/x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,31 +28,41 @@
 
 	function updateActivityImage(prsnce: any) {
 		if ($presence !== undefined) {
-			if ($presence.activities.length > 1 && $presence.activities[1].assets.large_image) {
-				if ($presence.activities[1].assets.large_image.includes('spotify')) {
-					const largeImage1 = $presence.activities[1].assets.large_image.substring(
-						$presence.activities[1].assets.large_image.indexOf(':') + 1
-					);
-					largeActivityImage = 'https://i.scdn.co/image/' + largeImage1;
-					active1 = 'spotify';
-				} else {
-					const largeImage1 = $presence.activities[1].assets.large_image;
-					largeActivityImage =
-						'https://' + largeImage1.substring(largeImage1.indexOf('https/') + 6);
-					active1 = 'other';
+			if ($presence.activities.length > 1 && $presence.activities[1].assets) {
+				if (
+					$presence.activities[1].assets !== undefined &&
+					$presence.activities[1].assets.large_image !== undefined
+				) {
+					if ($presence.activities[1].assets.large_image.includes('spotify')) {
+						const largeImage1 = $presence.activities[1].assets.large_image.substring(
+							$presence.activities[1].assets.large_image.indexOf(':') + 1
+						);
+						largeActivityImage = 'https://i.scdn.co/image/' + largeImage1;
+						active1 = 'spotify';
+					} else {
+						const largeImage1 = $presence.activities[1].assets.large_image;
+						largeActivityImage =
+							'https://' + largeImage1.substring(largeImage1.indexOf('https/') + 6);
+						active1 = 'other';
+					}
 				}
 				if ($presence.activities.length > 2 && $presence.activities[2].assets.large_image)
-					if ($presence.activities[2].assets.large_image.includes('spotify')) {
-						const largeImage2 = $presence.activities[2].assets.large_image.substring(
-							$presence.activities[2].assets.large_image.indexOf(':') + 1
-						);
-						largeActivityImage2 = 'https://i.scdn.co/image/' + largeImage2;
-						active2 = 'spotify';
-					} else {
-						const largeImage2 = $presence.activities[2].assets.large_image;
-						largeActivityImage2 =
-							'https://' + largeImage2.substring(largeImage2.indexOf('https/') + 6);
-						active2 = 'other';
+					if (
+						$presence.activities[2].assets !== undefined &&
+						$presence.activities[1].assets.large_image !== undefined
+					) {
+						if ($presence.activities[2].assets.large_image.includes('spotify')) {
+							const largeImage2 = $presence.activities[2].assets.large_image.substring(
+								$presence.activities[2].assets.large_image.indexOf(':') + 1
+							);
+							largeActivityImage2 = 'https://i.scdn.co/image/' + largeImage2;
+							active2 = 'spotify';
+						} else {
+							const largeImage2 = $presence.activities[2].assets.large_image;
+							largeActivityImage2 =
+								'https://' + largeImage2.substring(largeImage2.indexOf('https/') + 6);
+							active2 = 'other';
+						}
 					}
 				console.log(largeActivityImage + ' ' + largeActivityImage2);
 			}
@@ -139,8 +149,9 @@
 							</div>
 							<!-- User Information -->
 							<div class="text-left px-2 flex flex-col content-center justify-center">
-								<h2 class="text-3xl">
-									{$presence.discord_user.username}
+								<h2 class="text-3xl bold">
+									<!-- {$presence.discord_user.username}-->
+									InvitedToHell
 								</h2>
 								<h2 class="text-2xl" style="color: {borderColor}">
 									{$presence.discord_status}
@@ -151,29 +162,35 @@
 						<!-- User Activity -->
 						{#if $presence.activities.length > 1}
 							<div class="px-6 py-4">
-								<p class="font-semibold text-purple mb-2 text-2xl text-left">
+								<p class="bold text-purple mb-2 text-2xl text-left">
 									{$presence.activities[1].name}
 								</p>
 
 								<div class="flex row-auto items-center">
 									{#if $presence.activities[1].assets.large_image}
-										<img class="" src={largeActivityImage} alt="" width="70" height="70" />
+										<img
+											class="rounded-lg"
+											src={largeActivityImage}
+											alt=""
+											width="70"
+											height="70"
+										/>
 									{/if}
 									<!-- User Activity Image -->
-									{#if $presence.activities[1].assets.large_image}
-										<div class="flex-col flex-auto pl-5">
+									{#if $presence.activities[1].assets}
+										<div class="flex-col flex-auto pl-5 rounded-lg">
 											{#if $presence.activities[1].state}
-												<p class="text-text text-lg text-left content-center">
+												<p class="regular text-text text-lg text-left content-center">
 													{$presence.activities[1].state}
 												</p>
 											{/if}
 											{#if $presence.activities[1].details}
-												<p class="text-text text-lg text-left content-center">
+												<p class="regular text-text text-lg text-left content-center">
 													{$presence.activities[1].details}
 												</p>
 											{/if}
 											{#if $presence.activities[1].timestamps && active1 === 'other'}
-												<p class="text-text text-sm text-left content-center">
+												<p class="regular text-text text-sm text-left content-center">
 													{time}
 												</p>
 											{/if}
@@ -181,17 +198,17 @@
 									{:else}
 										<div class="flex-col flex-auto">
 											{#if $presence.activities[1].state}
-												<p class="text-text text-lg text-left content-center">
+												<p class="regular text-text text-lg text-left content-center">
 													{$presence.activities[1].state}
 												</p>
 											{/if}
 											{#if $presence.activities[1].details}
-												<p class="text-text text-lg text-left content-center">
+												<p class="regular text-text text-lg text-left content-center">
 													{$presence.activities[1].details}
 												</p>
 											{/if}
 											{#if $presence.activities[1].timestamps && active1 === 'other'}
-												<p class="text-text text-sm text-left content-center">
+												<p class="regular text-text text-sm text-left content-center">
 													{time}
 												</p>
 											{/if}
@@ -200,35 +217,41 @@
 								</div>
 							</div>
 						{:else if $presence.activities.length === 1 && $presence.discord_status !== 'offline'}
-							<p class="pt-5 text-text text-xl text-left content-center px-6">
+							<p class="regular pt-5 text-text text-xl text-left content-center px-6">
 								Currently doing nothing.
 							</p>
 						{/if}
 
 						{#if $presence.activities.length > 2}
 							<div class="px-6 py-4">
-								<p class="font-semibold text-purple mb-2 text-2xl text-left">
+								<p class="bold text-purple mb-2 text-2xl text-left">
 									{$presence.activities[2].name}
 								</p>
 								<div class="flex row-auto items-center">
 									{#if $presence.activities[2].assets.large_image}
-										<img class="" src={largeActivityImage2} alt="" width="70" height="70" />
+										<img
+											class="rounded-lg"
+											src={largeActivityImage2}
+											alt=""
+											width="70"
+											height="70"
+										/>
 									{/if}
 									<!-- User Activity Image -->
-									{#if $presence.activities[2].assets.large_image}
+									{#if $presence.activities[2].assets}
 										<div class="flex-col flex-auto pl-5">
 											{#if $presence.activities[2].state}
-												<p class="text-text text-lg text-left content-center">
+												<p class="regular text-text text-lg text-left content-center">
 													{$presence.activities[2].state}
 												</p>
 											{/if}
 											{#if $presence.activities[2].details}
-												<p class="text-text text-lg text-left content-center">
+												<p class="regular text-text text-lg text-left content-center">
 													{$presence.activities[2].details}
 												</p>
 											{/if}
 											{#if $presence.activities[2].timestamps && active2 === 'other'}
-												<p class="text-text text-sm text-left content-center">
+												<p class="regular text-text text-sm text-left content-center">
 													{time}
 												</p>
 											{/if}
